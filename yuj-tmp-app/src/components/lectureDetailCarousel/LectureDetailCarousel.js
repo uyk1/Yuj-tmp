e21b8eabcfca494f9f8adca1b1833cc23a775d20
uyk1 <uyk0928@gmail.com>
2023-02-02 07:20:56 +0900

    캐러셀 작업 진행

--- yuj-tmp-app/src/components/lectureDetailCarousel/LectureDetailCarousel.js
+++ yuj-tmp-app/src/components/lectureDetailCarousel/LectureDetailCarousel.js
@@ -3,33 +3,33 @@ import React from 'react'
 const LectureDetailCarousel = () => {
     return (
         <div>
-            <div class="carousel w-96">
-                <div id="item1" class="carousel-item relative w-full">
-                    <img src="./assets/Sample.jpg" class="w-full" />
-                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#item3" class="btn btn-circle">❮</a>
-                        <a href="#item2" class="btn btn-circle">❯</a>
+            <div className="carousel w-96">
+                <div id="item1" className="carousel-item relative w-full">
+                    <img src="./assets/Sample.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#item3" className="btn btn-circle">❮</a>
+                        <a href="#item2" className="btn btn-circle">❯</a>
                     </div>
                 </div>
-                <div id="item2" class="carousel-item relative w-full">
-                    <img src="./assets/Sample2.jpg" class="w-full" />
-                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#item1" class="btn btn-circle">❮</a>
-                        <a href="#item3" class="btn btn-circle">❯</a>
+                <div id="item2" className="carousel-item relative w-full">
+                    <img src="./assets/Sample2.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#item1" className="btn btn-circle">❮</a>
+                        <a href="#item3" className="btn btn-circle">❯</a>
                     </div>
                 </div>
-                <div id="item3" class="carousel-item relative w-full">
-                    <img src="./assets/Sample3.jpg" class="w-full" />
-                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#item2" class="btn btn-circle">❮</a>
-                        <a href="#item1" class="btn btn-circle">❯</a>
+                <div id="item3" className="carousel-item relative w-full">
+                    <img src="./assets/Sample3.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#item2" className="btn btn-circle">❮</a>
+                        <a href="#item1" className="btn btn-circle">❯</a>
                     </div>
                 </div>
             </div>
-            <div class="flex justify-center w-96 py-2 gap-2">
-                <a href="#item1" class="btn btn-xs">1</a>
-                <a href="#item2" class="btn btn-xs">2</a>
-                <a href="#item3" class="btn btn-xs">3</a>
+            <div className="flex justify-center w-96 py-2 gap-2">
+                <a href="#item1" className="btn btn-xs">1</a>
+                <a href="#item2" className="btn btn-xs">2</a>
+                <a href="#item3" className="btn btn-xs">3</a>
             </div>
         </div>
     )
--- yuj-tmp-app/src/components/lectureItemCard/LectureItemCard.js
+++ yuj-tmp-app/src/components/lectureItemCard/LectureItemCard.js
@@ -3,26 +3,26 @@ import React from 'react'
 const LectureItemCard = () => {
     return (
         <div>
-            <div classname="card w-80 bg-base-100 shadow-xl">
-                <figure classname="relative">
+            <div className="card w-80 bg-base-100 shadow-xl">
+                <figure className="relative">
                     {/* a 태그는 차후 Link 태그 등으로 교체 */}
                     <a href=''> 
-                        <div classname="card-actions absolute top-4 left-6">
-                            <div classname="badge badge-outline bg-accent p-4 text-xs font-semibold rounded-xl" style={{ color: '#fff', border: '0' }}>Raja</div>
-                            <div classname="badge badge-outline bg-success p-4 text-xs font-semibold rounded-xl" style={{ color: '#fff', border: '0' }}>완료</div>
+                        <div className="card-actions absolute top-4 left-6">
+                            <div className="badge badge-outline bg-accent p-4 text-xs font-semibold rounded-xl" style={{ color: '#fff', border: '0' }}>Raja</div>
+                            <div className="badge badge-outline bg-success p-4 text-xs font-semibold rounded-xl" style={{ color: '#fff', border: '0' }}>완료</div>
                         </div>
                         <img src="./assets/Sample.jpg" alt="Card Image" />
                     </a>
                 </figure>
-                <div classname="card-body">
-                    <div classname='flex'>
-                        <a href='' classname='flex align-center'>
+                <div className="card-body">
+                    <div className='flex'>
+                        <a href='' className='flex align-center'>
                             <img src='./assets/YujMainLogo.svg' style={{ height: '14px', paddingRight: '7px' }} />
                             <div style={{ fontSize: '12px', fontWeight: 'bold' }}>요가소년</div>
                         </a>
                         <div></div>
                     </div>
-                    <p classname="card-title" style={{ fontSize: '16px' }}>
+                    <p className="card-title" style={{ fontSize: '16px' }}>
                         {/* 글자수 제한 및 대체 기능 구현 */}
                         속근육 강화를 위한 20분 파트별 요...
                     </p>
